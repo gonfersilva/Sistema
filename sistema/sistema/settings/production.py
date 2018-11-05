71f9c4296dcaee348c375f88096c6a48367078ee
Gonçalo Silva <gonfersilva@gmail.com>
2018-11-05 14:07:02 +0000

    production apps

--- sistema/sistema/settings/production.py
+++ sistema/sistema/settings/production.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'producao', 
+    'users',
+    'bootstrap3',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

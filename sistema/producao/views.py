8c180df056de993e1c78ae049e697926c8c7ccba
Gonçalo Silva <goncalo.silva@elastictek.com>
2019-04-01 11:46:51 +0100

    erwgf4erg

--- sistema/producao/views.py
+++ sistema/producao/views.py
@@ -410,17 +410,7 @@ def create_bobinagem_retrabalho(request):
                 elif not Bobinagem.objects.filter(nome=b.nome).exists():     
                     instance.user = request.user
                     instance.save()
-                    data = instance.data
-                    data = data.strftime('%Y%m%d')
-                    map(int, data)
-                    if instance.num_bobinagem < 10:
-                        instance.nome = '4%s-0%s' % (data[1:], instance.num_bobinagem)
-                    else:
-                        instance.nome = '4%s-%s' % (data[1:], instance.num_bobinagem)
-                    instance.save()
-                    area_bobinagem(instance.pk) 
-                    create_bobine(instance.pk)  
-                    # bobinagem_create_retrabalho(instance.pk)
+                    bobinagem_create_retrabalho(instance.pk)
                     if not instance.estado == 'LAB' or instance.estado == 'HOLD':
                         areas(instance.pk)
                 
@@ -428,19 +418,7 @@ def create_bobinagem_retrabalho(request):
         else:
             instance.user = request.user
             instance.save()
-            # bobinagem_create_retrabalho(instance.pk)
-           
-            data = instance.data
-            data = data.strftime('%Y%m%d')
-            map(int, data)
-            if instance.num_bobinagem < 10:
-                instance.nome = '4%s-0%s' % (data[1:], instance.num_bobinagem)
-            else:
-                instance.nome = '4%s-%s' % (data[1:], instance.num_bobinagem)
-            instance.save()
-            area_bobinagem(instance.pk) 
-            create_bobine(instance.pk)  
-
+            bobinagem_create_retrabalho(instance.pk)
             if not instance.estado == 'LAB' or instance.estado == 'HOLD':
                 areas(instance.pk)
         

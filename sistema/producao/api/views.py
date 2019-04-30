a7e6d6c32a81b9e483225872063cce3a964c397a
Gonçalo Silva <goncalo.silva@elastictek.com>
2019-04-30 12:37:55 +0100

    Ordem Paletes

--- sistema/producao/api/views.py
+++ sistema/producao/api/views.py
@@ -114,7 +114,7 @@ class EncomendaCargaAPIView(LoginRequiredMixin, APIView):
 class CargaPaletesAPIView(LoginRequiredMixin, APIView):
     def get(self, request, pk, format=None):
         carga = Carga.objects.get(pk=pk)
-        paletes = Palete.objects.filter(carga=carga)
+        paletes = Palete.objects.filter(carga=carga).order_by('nome')
         serializer = PaletesCargaSerializer(paletes, many=True)
         return Response(serializer.data)
 
